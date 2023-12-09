4e4d532345d8602f44080bdda8d23a7c2918b764
Muawiyya Modibbo <modibbomm@gmail.com>
2023-12-09 13:37:09 +0100

    added cache files to ignore

--- Bunkering/obj/Debug/net7.0/Bunkering.AssemblyInfo.cs
+++ Bunkering/obj/Debug/net7.0/Bunkering.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bunkering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fda4efa0c83a26463d079d3b660a43304ff627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bunkering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bunkering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
