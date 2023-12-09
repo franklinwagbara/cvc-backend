9058fad54ba6ac4ea54539fee7d6f57766a3ee81
chichan999 <anyanwuchiamaka12@gmail.com>
2023-12-09 14:13:07 +0100

    commit

---
+++ Bunkering.Database/dbo/Tables/vAppUsers_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[vAppUsers] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Email]    NVARCHAR (MAX) NOT NULL,
+    [Location] NVARCHAR (MAX) NOT NULL,
+    [Office]   NVARCHAR (MAX) NOT NULL,
+    [StateId]  INT            NOT NULL,
+    [role]     NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_vAppUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
