f31aa2b4d84ba0343cea8fb5fe6944e2a8f10210
paul <paulwombo72@outlook.com>
2023-12-09 13:27:09 +0100

    Refactor apply method remove vessel type check

--- Bunkering.Access/obj/Debug/net7.0/Bunkering.Access.AssemblyInfo.cs
+++ Bunkering.Access/obj/Debug/net7.0/Bunkering.Access.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bunkering.Access")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92808f495a974c2351d7f4cd20182e30e12f60a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fda4efa0c83a26463d079d3b660a43304ff627")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bunkering.Access")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bunkering.Access")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
