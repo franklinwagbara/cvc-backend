b0b9de1fa6bb73ab4c1a99ecff1eeeb57320ead5
Franklin Godspower Wagbara <wagbarafranklin@yahoo.com>
2023-12-21 13:30:50 +0100

    update

--- Bunkering.Database/dbo/Tables/Depots.sql
+++ Bunkering.Database/dbo/Tables/Depots.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Depots] (
     [Id]   INT            IDENTITY (1, 1) NOT NULL,
     [Name] NVARCHAR (MAX) NOT NULL,
-    [State] NVARCHAR(50) NOT NULL, 
+    [StateId] INT NOT NULL, 
     [Capacity] DECIMAL NOT NULL, 
     [IsDeleted] BIT NOT NULL, 
     [DeletedAt] DATETIME NULL, 
