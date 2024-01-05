ebb7c419c137ec4d9ab3ffcdf0e9670f9dc6a487
Chiamaka Anyanwu <anyanwchiamaka12@gmail.com>
2024-01-05 11:08:33 +0100

    amaka

---
+++ Bunkering.Core/Migrations/20240104235054_RemoveIsFADFromSubmittedDocuments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bunkering.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIsFADFromSubmittedDocuments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFAD",
+                table: "SubmittedDocuments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFAD",
+                table: "SubmittedDocuments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
