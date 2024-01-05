83ee60fc4817cb99001ae410aa1393601274b08b
Mahmoud Wali <mahmoudmwali@outlook.com>
2024-01-05 17:23:14 +0100

    db update

--- Bunkering.Core/Migrations/20240105144926_ProductisDeleted.cs
+++ Bunkering.Core/Migrations/20240105144926_ProductisDeleted.cs
@@ -10,62 +10,20 @@ namespace Bunkering.Core.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_ApplicationDepots_Depots_DepotId",
-                table: "ApplicationDepots");
-
             migrationBuilder.AddColumn<bool>(
                 name: "IsDeleted",
                 table: "Products",
                 type: "bit",
                 nullable: false,
                 defaultValue: false);
-
-            migrationBuilder.AlterColumn<long>(
-                name: "ElpsPlantId",
-                table: "Plants",
-                type: "bigint",
-                nullable: true,
-                oldClrType: typeof(long),
-                oldType: "bigint");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_ApplicationDepots_Plants_DepotId",
-                table: "ApplicationDepots",
-                column: "DepotId",
-                principalTable: "Plants",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_ApplicationDepots_Plants_DepotId",
-                table: "ApplicationDepots");
-
             migrationBuilder.DropColumn(
                 name: "IsDeleted",
                 table: "Products");
-
-            migrationBuilder.AlterColumn<long>(
-                name: "ElpsPlantId",
-                table: "Plants",
-                type: "bigint",
-                nullable: false,
-                defaultValue: 0L,
-                oldClrType: typeof(long),
-                oldType: "bigint",
-                oldNullable: true);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_ApplicationDepots_Depots_DepotId",
-                table: "ApplicationDepots",
-                column: "DepotId",
-                principalTable: "Depots",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
         }
     }
 }
