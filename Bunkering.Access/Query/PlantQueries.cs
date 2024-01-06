52d7b2751666b45ea840ecfa8d59bfc6eae07a88
Muawiyya Modibbo <modibbomm@gmail.com>
2024-01-06 10:20:30 +0100

    updates on Endpoint

---
+++ Bunkering.Access/Query/PlantQueries.cs
@@ -0,0 +1,27 @@
+﻿using Bunkering.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunkering.Access.Query
+{
+    public class PlantQueries
+    {
+        private readonly ApplicationContext _context;
+        public PlantQueries(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Plant>> GetPlantsByCompanywithTanks(string email)
+        {
+            var plants = (from p in _context.Plants.AsQueryable()
+                        join pt in _context.PlantTanks on p.Id equals pt.PlantId
+                        where p.Email == email
+                        select p).ToList();
+            return plants;
+        }
+    }
+}
