acf13a8ad1b88c63605b0bab97a762dc16e6ba27
Muawiyya Modibbo <modibbomm@gmail.com>
2024-01-08 15:14:02 +0100

    Get Approved Coq

--- Bunkering/Controllers/API/CoQController.cs
+++ Bunkering/Controllers/API/CoQController.cs
@@ -147,7 +147,17 @@ namespace Bunkering.Controllers.API
 		[HttpGet]
 		public async Task<IActionResult> GetCoqs(int appId) => Response(await _coqService.GetCoQsByAppId(appId));
 
-		[ProducesResponseType(typeof(ApiResponse), 200)]
+        
+        [ProducesResponseType(typeof(ApiResponse), 200)]
+        [ProducesResponseType(typeof(ApiResponse), 404)]
+        [ProducesResponseType(typeof(ApiResponse), 405)]
+        [ProducesResponseType(typeof(ApiResponse), 500)]
+        [Produces("application/json")]
+        [Route("approved_coq")]
+        [HttpGet]
+        public async Task<IActionResult> GetApprovedCoQsByDepot() => Response(await _coqService.GetApprovedCoQsByFieldOfficer());
+
+        [ProducesResponseType(typeof(ApiResponse), 200)]
 		[ProducesResponseType(typeof(ApiResponse), 404)]
 		[ProducesResponseType(typeof(ApiResponse), 405)]
 		[ProducesResponseType(typeof(ApiResponse), 500)]
