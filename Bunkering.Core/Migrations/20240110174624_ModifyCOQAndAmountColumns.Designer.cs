40511a8db7ce90b7088a9f19bdaadcf4c3d0844c
paul <paulwombo72@outlook.com>
2024-01-10 18:54:17 +0100

    fix migration issues

--- Bunkering.Core/Migrations/20240110174624_ModifyCOQAndAmountColumns.Designer.cs
+++ Bunkering.Core/Migrations/20240110174624_ModifyCOQAndAmountColumns.Designer.cs
@@ -4,6 +4,7 @@ using Bunkering.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bunkering.Core.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240110174624_ModifyCOQAndAmountColumns")]
+    partial class ModifyCOQAndAmountColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
