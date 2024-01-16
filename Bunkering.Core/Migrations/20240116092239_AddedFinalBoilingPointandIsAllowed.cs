a5bee117736f67f3dd9d52e5cd7b7359a390edd3
Chiamaka Anyanwu <anyanwchiamaka12@gmail.com>
2024-01-16 10:39:14 +0100

    Added new properties to the vessel discharge clearance table

---
+++ Bunkering.Core/Migrations/20240116092239_AddedFinalBoilingPointandIsAllowed.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bunkering.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFinalBoilingPointandIsAllowed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VesselDischargeClearances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppId = table.Column<int>(type: "int", nullable: false),
+                    DepotId = table.Column<int>(type: "int", nullable: false),
+                    DischargeId = table.Column<int>(type: "int", nullable: false),
+                    VesselName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VesselPort = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Product = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Density = table.Column<double>(type: "float", nullable: false),
+                    RON = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FlashPoint = table.Column<double>(type: "float", nullable: false),
+                    FinalBoilingPoint = table.Column<double>(type: "float", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Odour = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Oxygenate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Others = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAllowed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VesselDischargeClearances", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VesselDischargeClearances");
+        }
+    }
+}
