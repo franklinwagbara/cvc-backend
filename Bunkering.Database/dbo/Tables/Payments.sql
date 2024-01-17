da054f79b5d9842a2a2592da0e91449741f4ddf1
dammielair <drayprogrammer45@yahoo.com>
2024-01-17 20:08:28 +0100

    ---new update on db scema

--- Bunkering.Database/dbo/Tables/Payments.sql
+++ Bunkering.Database/dbo/Tables/Payments.sql
@@ -8,14 +8,14 @@
     [PaymentType]        NVARCHAR (MAX) NOT NULL,
     [TransactionDate]    DATETIME2 (7)  NOT NULL,
     [PaymentDate]        DATETIME2 (7)  NOT NULL,
-    [TransactionId]      NVARCHAR (MAX) NOT NULL,
+    [TransactionId]      NVARCHAR (MAX) NULL,
     [RRR]                NVARCHAR (MAX) NOT NULL,
     [Description]        NVARCHAR (MAX) NOT NULL,
     [AppReceiptId]       NVARCHAR (MAX) NOT NULL,
     [Amount]             FLOAT (53)     NOT NULL,
     [Arrears]            FLOAT (53)     NULL,
     [ServiceCharge]      FLOAT (53)     NOT NULL,
-    [TxnMessage]         NVARCHAR (MAX) NOT NULL,
+    [TxnMessage]         NVARCHAR (MAX) NULL,
     [RetryCount]         INT            NOT NULL,
     [LastRetryDate]      DATETIME2 (7)  NOT NULL,
     [Account]            NVARCHAR (MAX) NOT NULL,
@@ -23,10 +23,10 @@
     [LateRenewalPenalty] FLOAT (53)     NOT NULL,
     [NonRenewalPenalty]  FLOAT (53)     NOT NULL,
     [Status]             NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_Payments] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Payments_Applications_ApplicationId] FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[Applications] ([Id]),
-    CONSTRAINT [FK_Payments_ExtraPayments_ExtraPaymentId] FOREIGN KEY ([ExtraPaymentId]) REFERENCES [dbo].[ExtraPayments] ([Id])
+    CONSTRAINT [PK_Payments] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
