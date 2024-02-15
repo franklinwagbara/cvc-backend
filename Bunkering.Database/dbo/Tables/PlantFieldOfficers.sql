3e668b6ad3de7824d3d66da2d204cb9194509d97
dammielair <drayprogrammer45@yahoo.com>
2024-02-15 21:18:40 +0100

    --new update

--- Bunkering.Database/dbo/Tables/PlantFieldOfficers.sql
+++ Bunkering.Database/dbo/Tables/PlantFieldOfficers.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [dbo].[PlantFieldOfficers] (
-    [ID]        INT              IDENTITY (1, 1) NOT NULL,
-    [PlantID]   INT              NOT NULL,
-    [OfficerID] UNIQUEIDENTIFIER NOT NULL,
-    [IsDeleted] BIT              NOT NULL,
+    [ID]        INT            IDENTITY (1, 1) NOT NULL,
+    [PlantID]   INT            NOT NULL,
+    [OfficerID] NVARCHAR (MAX) NOT NULL,
+    [IsDeleted] BIT            NOT NULL,
     CONSTRAINT [PK_PlantFieldOfficers] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_PlantFieldOfficers_Plants_PlantID] FOREIGN KEY ([PlantID]) REFERENCES [dbo].[Plants] ([Id]) ON DELETE CASCADE
 );
+
+
 
 
 GO
