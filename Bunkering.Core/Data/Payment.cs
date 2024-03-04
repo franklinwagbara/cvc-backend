3b80290fe4f6e342dadf61a9b9958f7dc69254fd
dammielair <drayprogrammer45@yahoo.com>
2024-03-04 10:50:17 +0100

    ---new update

--- Bunkering.Core/Data/Payment.cs
+++ Bunkering.Core/Data/Payment.cs
@@ -16,7 +16,8 @@ namespace Bunkering.Core.Data
 		public DateTime? PaymentDate { get; set; }
 		public string? TransactionId { get; set; }
 		public string? RRR { get; set; }
-		public string Description { get; set; }
+        public double? DebitNoteAmount { get; set; }
+        public string Description { get; set; }
 		public string? AppReceiptId { get; set; }
 		public double Amount { get; set; }
 		public double? Arrears { get; set; }
